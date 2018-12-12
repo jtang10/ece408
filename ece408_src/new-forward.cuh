ef49e8e6e73e5acda25220de446f4f2381afcd22
Jingning Tang <tangjingning@gmail.com>
2018-12-12 16:43:04 -0600

    granularity working, 2: 118ms; 3: 112ms; 4:108ms, cur 4

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -7,6 +7,7 @@
 #define MAX_NUM_THREADS 1024
 #define TILE_WIDTH  16
 #define TILE_HEIGHT 16
+#define GRANULARITY 4
 
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
@@ -27,15 +28,18 @@ namespace mxnet
 namespace op 
 {
 __global__ void forward_shared_unroll(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K, const int H_out, const int W_out) {
-  __shared__ float shmem_X[TILE_WIDTH][TILE_WIDTH*2];
+  __shared__ float shmem_X[TILE_WIDTH][TILE_WIDTH * GRANULARITY];
   __shared__ float shmem_K[TILE_WIDTH][TILE_WIDTH];
 
   int row = by * blockDim.y + ty;
-  int col = bx * blockDim.x * 2 + tx;
+  int col = bx * blockDim.x * GRANULARITY + tx;
   int numMatACol = C * K * K;
 
-  float acc = 0;
+  float acc  = 0;
   float acc2 = 0;
+  float acc3 = 0;
+  float acc4 = 0;
+
   int temp_col = tx;
   int temp_row = ty;
 
@@ -44,6 +48,12 @@ __global__ void forward_shared_unroll(float *y, const float *x, const float *k, 
 
   int X_h2 = (col + TILE_WIDTH) / W_out;
   int X_w2 = (col + TILE_WIDTH) % W_out;
+
+  int X_h3 = (col + TILE_WIDTH*2) / W_out;
+  int X_w3 = (col + TILE_WIDTH*2) % W_out;
+
+  int X_h4 = (col + TILE_WIDTH*3) / W_out;
+  int X_w4 = (col + TILE_WIDTH*3) % W_out;
 
   int K_c  = getC(temp_col);
   int K_k1 = getK1(temp_col);
@@ -54,6 +64,8 @@ __global__ void forward_shared_unroll(float *y, const float *x, const float *k, 
   float temp_k = k4d(row, K_c, K_k1, K_k2);
   float temp_x  = x4d(bz, X_c, X_h + X_p,  X_w + X_q);
   float temp_x2 = x4d(bz, X_c, X_h2 + X_p, X_w2 + X_q);
+  float temp_x3 = x4d(bz, X_c, X_h3 + X_p, X_w3 + X_q);
+  float temp_x4 = x4d(bz, X_c, X_h4 + X_p, X_w4 + X_q);
 
   #pragma unroll
   for (int i = 0; i < (numMatACol + TILE_WIDTH - 1) / (TILE_WIDTH); ++i) {
@@ -74,6 +86,19 @@ __global__ void forward_shared_unroll(float *y, const float *x, const float *k, 
     } else {
       shmem_X[ty][tx + TILE_WIDTH] = 0;
     }
+
+    if (temp_row < numMatACol && col + TILE_WIDTH * 2 < H_out * W_out) {
+      shmem_X[ty][tx + TILE_WIDTH * 2] = temp_x3;
+    } else {
+      shmem_X[ty][tx + TILE_WIDTH * 2] = 0;
+    }
+
+    if (temp_row < numMatACol && col + TILE_WIDTH * 3 < H_out * W_out) {
+      shmem_X[ty][tx + TILE_WIDTH * 3] = temp_x4;
+    } else {
+      shmem_X[ty][tx + TILE_WIDTH * 3] = 0;
+    }
+
     __syncthreads();
 
     temp_col += TILE_WIDTH;
@@ -87,19 +112,18 @@ __global__ void forward_shared_unroll(float *y, const float *x, const float *k, 
     temp_k = k4d(row, K_c, K_k1, K_k2);
     temp_x  = x4d(bz, X_c, X_h + X_p,  X_w + X_q);
     temp_x2 = x4d(bz, X_c, X_h2 + X_p, X_w2 + X_q);
+    temp_x3 = x4d(bz, X_c, X_h3 + X_p, X_w3 + X_q);
+    temp_x4 = x4d(bz, X_c, X_h4 + X_p, X_w4 + X_q);
 
     #pragma unroll
     for (int q = 0; q < TILE_WIDTH; ++q) {
       acc  += shmem_K[ty][q] * shmem_X[q][tx];
       acc2 += shmem_K[ty][q] * shmem_X[q][tx + TILE_WIDTH];
+      acc3 += shmem_K[ty][q] * shmem_X[q][tx + TILE_WIDTH*2];
+      acc4 += shmem_K[ty][q] * shmem_X[q][tx + TILE_WIDTH*3];
     }
 
     __syncthreads();
-
-    // int Y_b = b;
-    // int Y_m = row;
-    // int Y_h = col / W_out;
-    // int Y_w = col % W_out;
 
     if (row < M && col < W_out * H_out) {
       y4d(bz, row, X_h, X_w) = acc;
@@ -107,6 +131,14 @@ __global__ void forward_shared_unroll(float *y, const float *x, const float *k, 
 
     if (row < M && col + TILE_WIDTH < W_out * H_out) {
       y4d(bz, row, X_h2, X_w2) = acc2;
+    }
+
+    if (row < M && col + TILE_WIDTH * 2 < W_out * H_out) {
+      y4d(bz, row, X_h3, X_w3) = acc3;
+    }
+
+    if (row < M && col + TILE_WIDTH * 3 < W_out * H_out) {
+      y4d(bz, row, X_h4, X_w4) = acc4;
     }
   }
 }
@@ -147,7 +179,7 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y,
   const int W_out = W - K + 1;
 
   // newest kernel based on exam2
-  dim3 gridDim(ceil(1.0*H_out*W_out/TILE_WIDTH), ceil(1.0*M/TILE_WIDTH), B);
+  dim3 gridDim(ceil(1.0*H_out*W_out/TILE_WIDTH/GRANULARITY), ceil(1.0*M/TILE_WIDTH), B);
   dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
   forward_shared_unroll<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K, H_out, W_out);
   // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
